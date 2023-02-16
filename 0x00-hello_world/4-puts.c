3deaf5e82462c7535edfc55f93d7a4dc151bc8a7
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-16 10:00:37 -0800

    changes in Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
   /*Declaration Process*/
-  puts("Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
 
   return(0);
 }
