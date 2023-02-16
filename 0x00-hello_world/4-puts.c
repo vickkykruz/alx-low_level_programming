6740d0c9d886f4a421a365e1e645f0715d22fcdb
VickkyProgrammer <73972088+vickkykruz@users.noreply.github.com>
2023-02-16 16:00:39 +0100

    Changes in Task Four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@ int main(void)
   /*Declaration Process*/
   char output1[50];
 
-  strcpy(output1, "Programming is like building a multilingual puzzle\n");
+  strcpy(output1, "Programming is like building a multilingual puzzle");
   puts(output1);
 
   return(0);
