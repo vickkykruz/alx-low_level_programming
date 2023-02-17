eec58e68033d7442485d8cc43c7109d34855ba0a
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-17 10:24:51 -0800

    initals changes in Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - starting point
+ * description - It display every single digits
+ * @n - it store the vaule and diplay it
+ * Return: 0
+ */
 int main(void)
 {
 int n = 0;
 while (n < 10)
 {
-printf("%d\n", n);
+printf("%d", n);
 n++;
 }
+putchar('\n');
 return (0);
 }
