15ae5afcc42ec7e1d43bc8221e203c7155cfaaa0
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-17 08:06:45 -0800

    initals changes in task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -29,7 +29,7 @@ printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
 }
 else if (last_digit == 0)
 {
-printf("Last digit of %i is 0 and is 0\n", n);
+printf("Last digit of %i is %i and is 0\n", n, last_digit);
 }
 else
 {
