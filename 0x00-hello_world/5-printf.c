f70a0ec8f02fbb9c1b2bcc7978dcfea23333b4b5
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-17 05:44:55 -0800

    initals changes in task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h> 
-/*
-* main - Entry point
-* Return(0)
-*/
-int main(void) 
- { 
-        printf("with proper grammar, but the outcome is a piece of art,\n"); 
-         return (0);
- }
- 
+#include <stdio.h>
+/**
+ * main - starting function
+ * Description: main function deplays the output string
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
