53392c04312574e04a14b140b899cf8e11cf876d
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-18 05:12:38 -0800

    inital changes in task 13

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - starting point
+ * Description - This print numbers in a two single digit from 01 - 89 with a ',' and ' '
+ * @n1 - This is a vraiable that represent ones
+ * @n2 - This is a variable that represent tens
+ * section headers - stdio.h, stlib.h
+ * Return: 0
+ */
 int main(void)
 {
 int n1 = '0';
 int n2 = '0';
-for (n2; n2 <= '9'; n2++)
+for (n2 = '0'; n2 <= '9'; n2++)
 {
-for (n1; n1 <= '9'; n1++)
+for (n1 = '0'; n1 <= '9'; n1++)
 {
 if (!((n1 == n2) || (n2 > n1)))
 {
