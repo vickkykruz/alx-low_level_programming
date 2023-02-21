0c54a3b0c848a58fa9dfaa27bac4b9744bccbc7d
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-21 03:25:50 -0800

    initials changes in task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,32 @@
 /**
  * main - starting point
  * Description - print the output _putchar
+ * @word: Is used here to pass the agreement
  * section headers - stdio.h
  * Return: 0
  */
+/* Function Declaration */
+void print_putchar(char *word);
 int main(void)
 {
-printf("_putchar\n");
+char word[] = "_putchar\n";
+print_putchar(word);
+putchar('\n');
+
 return (0);
+}
+/**
+ * print_putchar - Check and print the string
+ * @word: is the variable that store sting
+ * Return: void
+ */
+/* Create the function */
+void print_putchar(char *word)
+{
+int i = 0;
+while (word[i] != '\n' && word[i] != '\0')
+{
+putchar(word[i]);
+i++;
+}
 }
