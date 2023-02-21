313818c59d00e746510eea2c7e69595a637ac1bd
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-21 06:01:59 -0800

    changes in task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,33 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - starting point
  * Description - print the output _putchar
  * @word: Is used here to pass the agreement
- * section headers - stdio.h
+ * section headers - main.h
  * Return: 0
  */
-/* Function Declaration */
-void print_putchar(char *word);
 int main(void)
 {
-char word[] = "_putchar\n";
-print_putchar(word);
-putchar('\n');
+_putchar('-');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-return (0);
-}
-/**
- * print_putchar - Check and print the string
- * @word: is the variable that store sting
- * Return: void
- */
-/* Create the function */
-void print_putchar(char *word)
-{
-int i = 0;
-while (word[i] != '\n' && word[i] != '\0')
-{
-putchar(word[i]);
-i++;
-}
+ return (0);
 }
