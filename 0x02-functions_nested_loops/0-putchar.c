44e5c860f82473a0f5c9330a6187532863a3f3af
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-21 02:09:43 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * Description - print the output _putchar
+ * section headers - stdio.h
+ * Return: 0
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
