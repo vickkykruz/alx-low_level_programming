ce72f17756cace06dc945248d5003da902bc4e4f
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-22 04:36:31 -0800

    inital changes in task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,13 +5,14 @@
  * Description: _islpha
  * Check the letter within the range of uppre and lower
  * cases
+ * @c: Checker input
  * Return: 1 if is a letter of lower & upper cases else 0
 */
 int _isalpha(int c)
 {
-if ((c >= 65) && (c <= 122))
+if (c >= 65 && c <= 122)
 {
-  return (1);
+return (1);
 }
 else
 {
