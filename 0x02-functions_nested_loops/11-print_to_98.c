0c9623d3642a9ff42c5f75bef0500e716e331bd6
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-23 00:48:07 -0800

    initails changes in task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,20 +7,45 @@
 */
 void print_to_98(int n)
 {
-if (n < 98)
+int i;
+if (n >= 0 && n <= 98)
 {
-for (n = n; n < 98; n++)
+for (i = n; i < 98; i++)
 {
-printf("%d, ", n);
-printf("%d\n", 98);
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
 }
 }
-else
-{
-for (n = n; n > 98; n--)
-{
-printf("%d, ", n);
-printf("%d\n", 98);
+printf("\n");
 }
+else if (n < 0)
+{
+for (i = n; i <= 98; i++)
+{
+printf("%d, ", i);
+if(i != 98)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
+else if (n > 98)
+{
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
+else if (n == 98)
+{
+printf("%d", n);
 }
 }
