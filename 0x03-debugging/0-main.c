ae94a9f13bc98c32f83672fae15f0424b00d7b62
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-23 02:36:04 -0800

    tasks

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
- * main - tests function thst print if integer is positive or negative
- * Return:   0
+ * main - tests function thst prints if integer is positive or negative
+ * Return: give 0
 */
+
 int main(void)
 {
 int i;
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
+
