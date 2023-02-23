aa181cf4d68d24196282518727e79e7907f0e0fe
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-22 23:54:56 -0800

    initail changes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,40 +5,40 @@
 */
 void times_table(void)
 {
-  int num1, num2, result;
-  for (num1 = 0; num1 <= 9; num1++)
-    {
-      for (num2 = 0; num2 <= 9; num2++)
-	{
-	  result = num1 * num2;
-	  if ((result / 10) == 0)
-	    {
-	      if (num2 == 0)
-		{
-		  _putchar('0');
-		}
-		if (num2 != 0)
-		  {
-		    _putchar(' ');
-		    _putchar((result % 10) + '0');
-		  }
-		if (num2 < 9)
-		  {
-		    _putchar(',');
-		    _putchar(' ');
-		  }
-	    }
-	  else
-	    {
-	      _putchar((result / 10) + '0');
-	      _putchar((result % 10) + '0');
-	      if (num2 < 9)
-		{
-		  _putchar(',');
-		  _putchar(' ');
-		}
-	    }
-	}
-      _putchar('\n');
-    }
-}		      
+int num1, num2, result;
+for (num1 = 0; num1 <= 9; num1++)
+{
+for (num2 = 0; num2 <= 9; num2++)
+{
+result = num1 * num2;
+if ((result / 10) == 0)
+{
+if (num2 == 0)
+{
+_putchar('0');
+}
+if (num2 != 0)
+{
+_putchar(' ');
+_putchar((result % 10) + '0');
+}
+if (num2 < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+else
+{
+_putchar((result / 10) + '0');
+_putchar((result % 10) + '0');
+if (num2 < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+_putchar('\n');
+}
+}
