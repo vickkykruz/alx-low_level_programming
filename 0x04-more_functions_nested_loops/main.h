342bb7fd81a82f10cb0b789122c5707a1573de97
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-24 01:44:16 -0800

    Task 3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MORE_FUNCTIONS_NESTED_LOOPS
 #define MORE_FUNCTIONS_NESTED_LOOPS
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 #endif
