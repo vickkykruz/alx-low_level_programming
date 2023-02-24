3a98a139ed6cc633ed618d7eade74041e9349647
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-24 01:30:25 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <math.h>
+/**
+ * mul - multiplies two numbers
+ * @a: The input value for a
+ * @b: The input value for b
+ * Return: result
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define MORE_FUNCTIONS_NESTED_LOOPS
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
