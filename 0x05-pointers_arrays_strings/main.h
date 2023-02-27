bd7bd77d7281fe48bef500c9f95f425bd78db535
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-02-27 02:18:22 -0800

    Task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef POINTERS_ARRAYS_STRINGS
 #define POINTERS_ARRAYS_STRINGS
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 #endif
