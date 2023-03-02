5e0892288c73b3ce0b61f8a2e19a0477c7b8d6e8
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-03-02 02:22:54 -0800

    Task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot14 - encodes string using rot13
+ * @c: The input string
+ * Return: c
+ */
+char *rot13(char *c)
+{
+int num, i;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+num = 0;
+while (*(c + num) != '\0')
+{
+for (i = 0; i < 52; i++)
+{
+if (*(c + num) == alphabet[i])
+{
+*(c + num) = rot13[i];
+break;
+}
+}
+num++;
+}
+return (c);
+}
