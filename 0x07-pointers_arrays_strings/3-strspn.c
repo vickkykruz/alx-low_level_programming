628c49a5a0815398297664a454fa955beb767483
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-03-06 01:13:06 -0800

    Task 1 & 2

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: The input string
+ * @accept: The input target
+ * Return: The number of bytes
+*/
+
+unsigned int _strspn(char *s, char *accept)
+int i, num, corr;
+i = 0;
+corr = 0;
+
+while (s[i] != '\0')
+{
+for (num = 0; accept[j] != '\0' j++)
+{
+if (s[i] == accept[j])
+{
+corr++;
+break;
+}
+if (accept[j + 1] == '\0' && s[i] != accept[j])
+{
+return (corr);
+}
+}
+i++;
+}
+return (corr);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,7 @@
 #ifndef MORE_POINTERS_ARRAYS_STRINGS
 #define MORE_POINTERS_ARRAYS_STRINGS
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 #endif
