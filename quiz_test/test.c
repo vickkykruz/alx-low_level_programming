2b040b989951107c95ce36b39cd4e4f2f975cc5d
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-03-08 06:16:16 -0800

    testing

---
+++ quiz_test/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+  int i;
+   for (i = 48; i < 58; i++)
+    {
+      printf("%c", i);
+    }
+  
+  /*    i = 0;
+  while(i < 10)
+    {
+      printf("%d", i % 2);
+      i++;
+      } */ 
+  printf("\n");
+  return (0);
+}
