321312df60e41c4edc5fff863b1a002ff4799a29
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-03-10 11:18:53 -0800

    correction in task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,20 +8,20 @@
 */
 int main(int argc, char *argv[])
 {
-int counter, num, mult = 1;
-if (argc > 1)
-{
-for (counter = 1; counter < argc; counter++)
-{
-num = atoi(argv[counter]);
-mult *= num;
-}
-printf("%d\n", mult);
-}
-else
+int a, b, mult;
+
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
+ 
+
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+mult = a * b;
+
+printf("%d\n", mult);
+
 return (0);
 }
