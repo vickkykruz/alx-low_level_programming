7c4f170b4321bdc50c927f07f7f7a1e3570bfd40
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-03-10 10:51:18 -0800

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function that multiple two numbers
+ * @argc: the number of arguments passed
+ * @argv: the array of the arguments passed
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int counter, num, mult = 1;
+if (argc > 1)
+{
+for (counter = 1; counter < argc; counter++)
+{
+num = atoi(argv[counter]);
+mult *= num;
+}
+printf("%d\n", mult);
+}
+return (0);
+}
