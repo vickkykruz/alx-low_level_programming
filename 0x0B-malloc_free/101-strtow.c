e2362e22674c8cd257e149c7e77c24a155302314
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-03-14 14:39:57 -0700

    incomplete task

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strtow - function that splits a string into words.
+ * @str: The input string
+ * Return: The pointer
+*/
+
+char **strtow(char *str)
+{
+  char *ptr;
+  
