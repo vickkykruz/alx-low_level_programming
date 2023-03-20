dee9024aa4fc09ff16189d3d18ef2d97bb67c3c1
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-03-20 14:29:42 -0700

    Completed task 3

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - function that prints a struct dog
+ * @d: This in input variable of struct dog
+ * Description: a function that prints a struct dog
+*/
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+printf("nil\n");
+}
+
+if (d->name == NULL)
+{
+printf("Name: nil\n");
+}
+else
+{
+printf("Name: %s\n", d->name);
+}
+
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
