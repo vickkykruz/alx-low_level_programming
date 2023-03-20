37e0ead8072b301598f2dbaea1fc320f7c117851
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-03-20 08:49:23 -0700

    Completed task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - this function define the following elements
+ * @name: The name of the given dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ * Description: This function define the data type of the following element
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
