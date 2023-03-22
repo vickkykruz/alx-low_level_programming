43b5390bc7f16f73a6b4a65c55631e3c312d5bc1
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-03-22 05:36:44 -0700

    Completed task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name.
+ * @name: The person name
+ * @f: Respresent a call back function
+ * Return: Nothing
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	/* Here we are going to the function with an agrument name */
+	f(name);
+}
