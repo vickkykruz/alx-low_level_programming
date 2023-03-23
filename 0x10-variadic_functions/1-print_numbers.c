252e357ea9d8c616af05ffb6abe469ba2b2a1838
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-03-23 08:09:34 -0700

    Testing Some changes

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,8 +12,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	if (separator == NULL)
-		return (0);
+	/* if (separator == NULL)
+		return (0); */
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
