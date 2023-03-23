dcd768c4974cbc5d1caf481573f5cdfa04318cd4
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-03-23 07:42:47 -0700

    inital changes in task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 
 	if (separator == NULL)
-		return (NULL);
+		return (0);
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
