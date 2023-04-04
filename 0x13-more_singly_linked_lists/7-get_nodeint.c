514ef7a8f11b396021d27b00850462cc22b641d7
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-04-03 19:10:01 -0700

    little changes made

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	size_t counter = 0;
+	unsigned int counter = 0;
 	listint_t *temp = head;
 
 	while (temp && counter < index)
