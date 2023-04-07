549e550ea904295897c05c522c4a6b6851bbbecc
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-04-07 07:55:21 -0700

    Betty correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,15 +21,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
-
 	/** we want to allocate the memory to read input **/
 	c = (char *) malloc(sizeof(char) * letters);
 	if (c == NULL)
-	{
-		close(fd);
 		return (0);
-	}
-
 	/** We will read the fie into the memory **/
 	read_no = read(fd, c, letters);
 	if (read_no < 0)
@@ -37,9 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(c);
 		return (0);
 	}
-
 	close(fd);
-
 	/** We will write from he memory to the file **/
 	write_no = write(STDOUT_FILENO, c, letters);
 	free(c);
@@ -50,11 +43,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	if (read_no != write_no)
-	{
 		return (0);
-	}
 
-	/* free(c); */
-	/* close(fd); */
 	return (write_no);
 }
