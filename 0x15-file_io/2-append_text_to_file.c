df6f9a1e96760f4729929a5b5b54902823a69acf
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-04-07 07:25:59 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * _strlen - A function that give you the total length of a string
+ * @c: The given string
+ *
+ * Return: i
+ *
+*/
 int _strlen(const char *c)
 {
 	int i;
@@ -17,21 +24,29 @@ int _strlen(const char *c)
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, len_w;
+	int fd, len_w, f_check;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY |  O_APPEND);
-	if (fd < 0)
-		return (-1);
-	/* CHECK FOR THE text_content **/
-	if (text_content)
+	f_check = access(filename, F_OK);
+	if (f_check == 0)
 	{
-		len_w = write(fd, text_content, _strlen(text_content));
-		if (len_w < 0)
+		fd = open(filename, O_WRONLY |  O_APPEND);
+		if (fd < 0)
 			return (-1);
+		/* CHECK FOR THE text_content **/
+		if (text_content)
+		{
+			len_w = write(fd, text_content, _strlen(text_content));
+			if (len_w < 0)
+				return (-1);
+		}
+		close(fd);
+		return (1);
 	}
-	close(fd);
-	return (1);
+	else
+	{
+		return (-1);
+	}
 }
