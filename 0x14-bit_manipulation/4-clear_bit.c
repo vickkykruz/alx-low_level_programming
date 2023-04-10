13945c1f60f2e889123c433235a9ac448faa816c
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-04-10 00:51:29 -0700

    Remove the equal sign

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * clear_bit - A function that sets the value of a bit to 0 at a given index.
+ * @n: The given n value
+ * @index: is the index, starting from 0 of the bit you want to set
+ *
+ * Return: 1 or -1
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	/** Declaration of varaible **/
+	int mask;
+
+	if (!n || index > 53)
+		return (0);
+
+	/** Mask C operatiom */
+	int = maskimg
+		return Lp
