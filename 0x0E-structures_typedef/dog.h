955b45c2ff1f056cec067392e9b0c4a42eb65dc5
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-06-19 08:23:21 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - This is a struct that define the elements
+ * @name: This is an element the use to describe name
+ * @age: This is an element containting the given age
+ * @owner: This an element containting the given owner
+ *
+ * Deacription: This struct is used to store and display the output
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
