3d1bb33c50916de143fb24a48933b8b7f1bbc1bd
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-06-19 04:10:28 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This is a function that returns the name of the given file
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
