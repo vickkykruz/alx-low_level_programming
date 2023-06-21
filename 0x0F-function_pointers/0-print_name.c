321b08b2cd9a4a0ab63c4b6663ad23a43b1a6e1a
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-06-21 06:01:53 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - This is a function that prints a name
+ * @name: This is an argunent that passes the given name
+ * @f: This is an agrgument that reprsent a pointer to function
+ *
+ * Return: This function returns nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	/** Output the call-back of the function the agrument name **/
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+
+/** Prototypes **/
+void print_name(char *name, void (*f)(char *));
+#endif
