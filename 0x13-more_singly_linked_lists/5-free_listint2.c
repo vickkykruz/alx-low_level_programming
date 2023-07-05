253793f3a89e4d8125a532df1930209fde496f3d
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-07-05 10:45:08 -0700

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - This is a function that frees a listint_t list.
+ * @head: This is an argument that represent the HEAD of the struct list
+ *
+ * Return: This function is a void(it return nothing)
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (*head == NULL)
+		return;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	*head = NULL;
+}
