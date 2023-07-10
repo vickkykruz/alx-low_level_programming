fb4142c10da0f6b5c6d80f8855a69db0861a79fe
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-07-10 10:10:28 -0700

    inital changes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,19 +11,21 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, i, lwr;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return  (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
-	for (i = 0; text_content[i]; i++)
-		;
-	lwr = write(fd, text_content, i);
-	if (lwr == -1)
-		return (-1);
-
+	if (text_content)
+	{
+		for (i = 0; text_content[i]; i++)
+			;
+		lwr = write(fd, text_content, i);
+		if (lwr == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
