cb65c654d0365666cc1c78306ac67ea2f836bb99
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-08-17 08:55:00 -0700

    made little changes

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,7 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		else
 		{
 			new_node = malloc(sizeof(dlistint_t));
-			if (new_node == NULL)
+			if (!new_node)
 				return (NULL);
 
 			new_node->n = n;
