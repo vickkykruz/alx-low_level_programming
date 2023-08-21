d7fb63e721318627ca0bfd8096bfad692c316a73
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-08-21 06:46:08 -0700

    Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/100-main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+/**
+ * add -This is an function that return the added number
+ * @num1: This is an argument that contain the first number
+ * @num2: This is an argument that contain the second number
+ *
+ * Return: This function return the added number
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+/**
+ * sub - This is a function that return the substracted number
+ * @num1: This is an argument that contain the first number
+ * @num2: This is an argument that contain the second number
+ *
+ * Return: This is a function that return the substract number
+ */
+int sub(int num1, int num2)
+{
+	return (num1 - num2);
+}
+/**
+ * mul - This is a function that return the multipled number
+ * @num1: This is an argument that reprsent the first number
+ * @num2: This is an argument that reprsent the second number
+ *
+ * Return: This function return the multipled number
+ */
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
+/**
+ * div - This is a function that return the divided number
+ * @num1: This is an argument that represent the first number
+ * @num2: This is an argument that reprsent the second number
+ *
+ * Return: This function return the divided number
+ */
+int div(int num1, int num2)
+{
+	return (num1 / num2);
+}
+/**
+ * mod - This is a function that return the modular of the number
+ * @num1: This is an argument that reprsent the first number
+ * @num2: This is an argument that represent the second number
+ *
+ * Return: This is a function that return the modular of the number
+ */
+int mod(int num1, int num2)
+{
+	return (num1 % num2);
+}
