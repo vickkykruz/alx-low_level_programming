65c360094391c970f4cae2928084d2d8af9e7524
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-09-08 13:43:03 -0700

    Made changes

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,36 +1,5 @@
 #include "hash_tables.h"
 /**
- * free_node - This is a function that the free the node
- * @head: This is an argument that reprsent the head nodof head_node_t struct
- *
- * Return: This function return a void (nothing)
- */
-void free_node(hash_node_t *node)
-{
-	free(node->key);
-	free(node->value);
-	free(node);
-}
-/**
- * free_list - This is a function that free the list
- * @head: This is an argument that reprsent the head node struct
- *
- * Return: This function return a void (nothing)
- */
-void free_list(hash_node_t *head)
-{
-	hash_node_t *tmp = head, *pre;
-
-	while (tmp)
-	{
-		pre = tmp;
-		tmp = tmp->next;
-
-		free_node(pre);
-	}
-	free(tmp);
-}
-/**
  * hash_table_delete - This is a function that deletes a hash table.
  * @ht: This is an argument that reprsent the table struct
  *
@@ -41,12 +10,21 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *tmp;
 	unsigned long int i;
 
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
 	for (i = 0; i < ht->size; i++)
 	{
-		tmp = ht->array[i];
-		if (!tmp)
-			continue;
-
-		free_list(tmp);
+		while (ht->array[i] != NULL)
+		{
+			tmp = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = tmp;
+		}
 	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
 }
