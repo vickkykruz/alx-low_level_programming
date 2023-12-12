e3fc6e3fbdd044d4a9c5e5b379f040113ad12dfc
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-12-12 01:57:49 -0800

    Jump Search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,8 +5,10 @@
 /** Include libraries **/
 #include <stdio.h>
 #include <stddef.h>
+#include <math.h>
 
 /*** Prototypes **/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 #endif
